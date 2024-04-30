f24c836a0bfe7a9323af3e4ad041e0a7bab17e13
aysucuk <aysusadixova@gmail.com>
2024-04-30 12:29:40 +0000

    file is modified

--- shell.c
+++ shell.c
@@ -1,16 +1,46 @@
 #include "main.h"
+#include <errno.h>
 
 /**
-  * main - Main entry point for our program
-  * @argc: Argument count to the main
-  * @argv: Pointer to array of argument values
-  *
-  * Return: O Always success
-  */
-
-int main(int argc, char *argv[])
+ * main - shell start function
+ * @argc: argument counter
+ * @argv: argument variables
+ * Return: always 0
+ */
+int main(int argc, char **argv)
 {
-        (void)argc, (void)argv;
-        write(STDOUT_FILENO, "MyShell$ ", 9);
-        return (0);
+	char *command;
+	char **fcommand = malloc(sizeof(char *) * 2);
+	size_t n = 0;
+	size_t k;
+	pid_t pid;
+	struct stat st;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		k = getline(&command, &n, stdin);
+		if (k == (size_t)(-1))
+		{
+			perror("Failure to read line");
+			break;
+		}
+		command[k] = '\0';
+		if (stat(command, &st) == -1)
+		{
+			printf("%s\n", command);
+			perror(argv[0]);
+			continue;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			fcommand = (char *[]) {command, NULL};
+			execve(command, fcommand, NULL);
+		}
+		else
+			wait(NULL);
+	}
+	return (0);
 }
+
