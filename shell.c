fe5fe16d14b645457285eb7918f04a0a09d1a3dd
aysucuk <aysusadixova@gmail.com>
2024-05-02 19:01:04 +0000

    shell file edited

--- shell.c
+++ shell.c
@@ -1,45 +1,70 @@
 #include "main.h"
-#include <errno.h>
-
 /**
- * main - shell start function
- * @argc: argument counter
- * @argv: argument variables
- * Return: always 0
- */
-int main(int argc, char **argv)
+  * main_helper - helper function for main
+  * @fcommand: tokenaized user command
+  * @status: status about echo
+  * @buf: user input
+  * Return: if break needed 1 else 0
+  */
+int main_helper(char **fcommand, int status, char *buf)
 {
-	char **fcommand;
-	size_t n = 0;
-	pid_t pid;
-	struct stat st;
-	int status;
-
-	while (1)
+	if (fcommand == NULL)
 	{
-		status = isatty(STDIN_FILENO);
+		free(buf), free_path();
 		if (status)
-			printf("#cisfun$ ");
-		else
-			errno = 0;
-		fcommand = get_command();
-		if (fcommand == NULL)
-		{
 			printf("\n");
-			break;
-		}
-		if (stat(fcommand[0], &st) == -1)
-		{
-			perror(argv[0]);
-			continue;
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(fcommand[0], fcommand, environ);
-		}
-		else
-			wait(NULL);
+		return (1);
+	}
+	else if (!strcmp(fcommand[0], "exit"))
+	{
+		free(buf), free_path(), free(fcommand);
+		return (1);
 	}
 	return (0);
+}
+/**
+  * main - shell start function
+  * @argv: argument variables
+  * @argc: argument count
+  * @env: env variables
+  * Return: always 0
+  */
+int main(int argc, char **argv, char **env)
+{
+	char **fcommand, *buf, *command;
+	int status, lk = 0, r_code = 0;
+
+	(void)argc, path_var = get_path(env);
+	while (1)
+	{
+		status = isatty(STDIN_FILENO), print_prompt(status);
+		fcommand = get_command(&buf);
+		if (main_helper(fcommand, status, buf))
+			break;
+		if (!strcmp(fcommand[0], " "))
+		{
+			free(buf), free(fcommand[0]), free(fcommand);
+			continue;
+		}
+		command = strdup(fcommand[0]);
+		if (!strcmp("env", fcommand[0]))
+		{
+			print_env(), free(buf), free(fcommand), free(command);
+			continue;
+		}
+		fcommand[0] = find_file(fcommand[0], &lk);
+		if (!fcommand[0])
+		{
+			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], command);
+			free(buf), free(fcommand), free(command), errno = 0;
+			if (!status)
+				free_path(), exit(127);
+			continue;
+		}
+		r_code = exec_c(fcommand), free(buf);
+		if (lk)
+			free(fcommand[0]), lk = 0;
+		free(fcommand), free(command);
+	}
+	return (r_code);
 }
