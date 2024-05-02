f6142e54d707298a924f8202a1497f0f245e3480
aysucuk <aysusadixova@gmail.com>
2024-05-02 18:53:09 +0000

    prototypes added to main file

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAINH
 #define MAINH
-
+#define PATH "PATH="
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +8,17 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
+char **path_var;
 extern char **environ;
-char **get_command(void);
+char **get_command(char **);
+char **get_path(char **);
+char *find_file(char *command, int *);
+void print_env(void);
+void print_prompt(int status);
+void free_path(void);
+int exec_c(char **fcommand);
+char **_strtok(char *str, char delim);
 
 #endif
