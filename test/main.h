71fe41ceb72a4a77d05d01d5fe0522517b5e3ba8
aysucuk <aysusadixova@gmail.com>
2024-05-02 10:43:26 +0000

    files created for test

---
+++ test/main.h
@@ -0,0 +1,15 @@
+#ifndef MAINH
+#define MAINH
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern char **environ;
+char **get_command(void);
+
+#endif
