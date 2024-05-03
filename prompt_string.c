0de67af7a49a29bd477fc5a119efcef4839e43e3
Atya22 <aytaj2002eva@gmail.com>
2024-05-03 17:32:48 +0000

    comments are added to prompt_string.c file

--- prompt_string.c
+++ prompt_string.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * print_prompt - prints prompt string
+ * @status: 1 if fd is an open file descriptor referring to a terminal
+ */
 void print_prompt(int status)
 {
 	if (status)
