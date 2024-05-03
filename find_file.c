f874dc0a20ed3ab7757a81834d645963cdd89ce4
Atya22 <aytaj2002eva@gmail.com>
2024-05-03 18:40:22 +0000

    find_file.c file is edited

--- find_file.c
+++ find_file.c
@@ -1,41 +1,61 @@
 #include "main.h"
 
 /**
- * find_file - finds file location in path variables
- * @command: user input
- * @lk: flag indicating if this command uses path or not
- *
- * Return: modified user input
+ * path_var - Array of path variables.
  */
-char **path_var = NULL;
+char **path_var;
+
+/**
+ * find_file - Finds file location in path variables.
+ *
+ * @command: User input specifying the file.
+ * @lk: Flag indicating whether this command uses path or not.
+ *
+ * Return: Modified user input.
+ */
 char *find_file(char *command, int *lk)
 {
-	int i = 0, len;
-	char *temp;
-	struct stat st;
+    int i = 0, max_len = 0, j;
+    char *temp, *result = NULL;
+    struct stat st;
 
-	if (command[0] == '/' || command[0] == '.')
-		if (!stat(command, &st))
-			return (command);
+    if (command[0] == '/' || command[0] == '.')
+    {
+        if (!stat(command, &st))
+        {
+            result = strdup(command);
+            if (result != NULL)
+                (*lk)++;
+        }
+        return (result);
+    }
 
-	if (!path_var)
-		return (NULL);
+    if (!path_var)
+        return (NULL);
 
-	while (path_var[i])
-	{
-		len = strlen(path_var[i]);
-		temp = strdup(path_var[i]);
-		temp = realloc(temp, len + strlen(command) + 2);
-		temp[len] = '/', temp[len + 1] = '\0';
-		temp = strcat(temp, command);
-		if (!stat(temp, &st))
-		{
-			(*lk)++;
-			errno = 0;
-			return (temp);
-		}
-		i++;
-		free(temp);
-	}
-	return (NULL);
+    for (j = 0; path_var[j] != NULL; j++)
+        max_len += strlen(path_var[j]) + strlen(command) + 2;
+
+    temp = malloc(max_len);
+    if (temp == NULL)
+        return (NULL);
+
+    while (path_var[i])
+    {
+        strcpy(temp, path_var[i]);
+        strcat(temp, "/");
+        strcat(temp, command);
+        if (!stat(temp, &st))
+        {
+            (*lk)++;
+            errno = 0;
+            result = strdup(temp);
+            break;
+        }
+        i++;
+    }
+
+    free(temp);
+    return (result);
 }
+
